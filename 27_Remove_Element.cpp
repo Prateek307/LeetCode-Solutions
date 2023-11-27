103c0d8e7f89b423f7548c2db155604dfdadd342
Prateek Kanaujia <152027702+Prateek307@users.noreply.github.com>
2023-11-28 01:45:52 +0530

    Add files via upload

---
+++ 27_Remove_Element.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, val;
+    cin >> n >> val;
+    vector<int> v(n);
+    for (auto &it : v)
+        cin >> it;
+    int cnt = 0;
+    for (auto it = v.begin(); it != v.end();)
+    {
+        if(*it==val)
+        { 
+            it = v.erase(it);
+        }
+        else
+        {
+            cnt++;
+            ++it;
+        }
+    }
+    for (auto it : v)
+    {
+        cout << it << ' ';
+    }
+    cout << '\n';
+    cout<<cnt<<'\n';
+    return 0;
+}
