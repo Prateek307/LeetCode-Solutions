5701c699d6738899b5d6cd250dbe920640d5bef3
Prateek Kanaujia <152027702+Prateek307@users.noreply.github.com>
2023-12-02 01:05:48 +0530

    Add files via upload

---
+++ 122_Best_time_to_buy_and_sell_stock_2.cpp
@@ -0,0 +1,16 @@
+// link to the problem ---> https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+       int profit = 0,i;
+       for(i=1;i<prices.size();i++)
+       {
+           if(prices[i]>prices[i-1])
+           {
+               profit+=prices[i]-prices[i-1];
+           }
+       } 
+       return profit;
+    }
+};
