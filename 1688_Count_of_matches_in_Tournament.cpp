8b6e5426ecdbaf31d6efdf331c7879f5f05b9611
Prateek Kanaujia <152027702+Prateek307@users.noreply.github.com>
2023-12-05 09:34:36 +0530

    Create 1688_Count_of_matches_in_Tournament.cpp

---
+++ 1688_Count_of_matches_in_Tournament.cpp
@@ -0,0 +1,7 @@
+// link to the problem ---> https://leetcode.com/problems/count-of-matches-in-tournament/description/?envType=daily-question&envId=2023-12-05
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        return n-1;
+    }
+};
