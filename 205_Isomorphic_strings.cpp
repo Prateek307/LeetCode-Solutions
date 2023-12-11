1a18c2e1cf658e03f5e1c8964990c8e18a018c2d
Prateek Kanaujia <152027702+Prateek307@users.noreply.github.com>
2023-12-11 21:31:23 +0530

    Add files via upload

---
+++ 205_Isomorphic_strings.cpp
@@ -0,0 +1,15 @@
+// link to the problem ---> https://leetcode.com/problems/isomorphic-strings/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution {
+public:
+ bool isIsomorphic(string s, string t) {
+    unordered_map<char,char> mp, mp2;
+    for(int i=0; i<s.size(); i++){
+        if(mp[s[i]] && mp[s[i]]!=t[i]) return false;
+        if(mp2[t[i]] && mp2[t[i]]!=s[i]) return false;
+        mp[s[i]] = t[i];
+        mp2[t[i]] = s[i];
+    }   
+    return true;
+ }
+};
