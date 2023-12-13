f7459545d09555744acb191b2a3e357d3583aff4
Prateek Kanaujia <152027702+Prateek307@users.noreply.github.com>
2023-12-13 14:20:19 +0530

    Add files via upload

---
+++ 167_Two_Sum2_input_array_is_sorted.cpp
@@ -0,0 +1,24 @@
+// link to the problem ---> https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int low = 0 , high = numbers.size()-1;
+        vector<int> ans;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(numbers[low] + numbers[high] == target)
+            { 
+                ans.push_back(low+1);
+                ans.push_back(high+1);
+                break;
+            }
+            else if(numbers[low] + numbers[high] > target)
+            high--;
+            else
+            low++;
+        }
+        return ans;
+    }
+};
