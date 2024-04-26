42cc7605c5f29ad0c6db77a9ab8101ac1049079b
Prateek Kanaujia <152027702+Prateek307@users.noreply.github.com>
2024-04-27 04:18:17 +0530

    Add files via upload

---
+++ 31_Next_Permutation.cpp
@@ -0,0 +1,8 @@
+// link to the problem ---> https://leetcode.com/problems/next-permutation/
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(), nums.end());
+    }
+};
