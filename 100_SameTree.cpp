6dc10e57e211678f0a96f065733a0c08abee9a1f
Prateek Kanaujia <152027702+Prateek307@users.noreply.github.com>
2024-05-10 07:18:26 +0530

    Add files via upload

---
+++ 100_SameTree.cpp
@@ -0,0 +1,20 @@
+// link to the problem ---> https://leetcode.com/problems/same-tree/
+
+class Solution
+{
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q)
+    {
+        if (!p && !q)
+        {
+            return true;
+        }
+
+        if (p && q && p->val == q->val)
+        {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+
+        return false;
+    }
+};
