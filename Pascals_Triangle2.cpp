a6a27af336a917977797f398d930f1a2d82f78fb
Prateek Kanaujia <152027702+Prateek307@users.noreply.github.com>
2024-05-23 02:21:30 +0530

    Add files via upload

---
+++ Pascals_Triangle2.cpp
@@ -0,0 +1,18 @@
+// link to the problem ---> https://leetcode.com/problems/pascals-triangle-ii/
+
+class Solution
+{
+public:
+    std::vector<int> getRow(int rowIndex)
+    {
+        std::vector<int> res(1, 1);
+        long long prev = 1;
+        for (int k = 1; k <= rowIndex; k++)
+        {
+            long long next_val = prev * (rowIndex - k + 1) / k;
+            res.push_back(next_val);
+            prev = next_val;
+        }
+        return res;
+    }
+};
