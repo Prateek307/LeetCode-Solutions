d02993aee19ef915f0086929f3870d3499f1cda2
Prateek Kanaujia <152027702+Prateek307@users.noreply.github.com>
2024-07-16 14:43:18 +0530

    Add files via upload

---
+++ 191_Number_of_1_Bits.cpp
@@ -0,0 +1,10 @@
+// link to the problem ---> class Solution {
+
+class Solution
+{
+public:
+    int hammingWeight(int n)
+    {
+        return __builtin_popcount(n);
+    }
+};
---
+++ 231_Power_of_Two.cpp
@@ -0,0 +1,14 @@
+// link to the problem ---> https://leetcode.com/problems/power-of-two/
+
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    {
+        if (n <= 0)
+            return 0;
+        else if ((n & (n - 1)) == 0)
+            return 1;
+        return 0;
+    }
+};
