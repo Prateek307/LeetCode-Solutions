b73869b6a4cb5838bb5e79e88af56bc2bbc7229d
Prateek Kanaujia <152027702+Prateek307@users.noreply.github.com>
2024-07-24 01:00:45 +0530

    Add files via upload

---
+++ 172_Factorial_Trailing_Zeroes.cpp
@@ -0,0 +1,17 @@
+// link to the problem ---> https://leetcode.com/problems/factorial-trailing-zeroes/
+
+class Solution
+{
+public:
+    int trailingZeroes(int n)
+    {
+        int count = 0;
+
+        while (n >= 5)
+        {
+            n /= 5;
+            count += n;
+        }
+        return count;
+    }
+};
